557f01e347e61e5bae9cbbf876a92ec7e07281e6
Eddie A Tejeda <eddie.tejeda@gmail.com>
2017-02-13 13:36:05 -0800

    add support for nested search to new master

--- lib/searchkick/query.rb
+++ lib/searchkick/query.rb
@@ -421,6 +421,17 @@ module Searchkick
 
         # filters
         filters = where_filters(options[:where])
+
+        # nested filter
+        if nested_filter_value = options[:nested]
+          filters << {
+            nested: {
+              path: nested_filter_value[:path],
+              filter: { and: where_filters(nested_filter_value[:where]) }
+            }
+          }
+        end
+
         set_filters(payload, filters) if filters.any?
 
         # aggregations
